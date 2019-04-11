c8620d6ff77bffa4e7d16b9fd21faf718a4bf384
alex <alexmir2015@yandex.ru>
2019-04-11 17:37:08 +0300

    Delete files

--- src/main/java/services/kohonenNetwork/KohonenNetwork.java
+++ src/main/java/services/kohonenNetwork/KohonenNetwork.java
@@ -5,4 +5,5 @@ import models.Entity;
 public interface KohonenNetwork {
     void train(Entity person) throws Exception;
     int handle(Entity person) throws Exception;
+    Neuron[] getConfiguration();
 }
